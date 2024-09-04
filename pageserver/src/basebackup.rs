4441515501521f2717baf5b2ca54c71509ef2375
Heikki Linnakangas <heikki@neon.tech>
2024-09-04 16:20:48 +0300

    Fix handling twophase files in v17
    Starting in v17, the filenames in pg_twophase directory use wide
    64-bit XIDs (32-bit previously). Commit 8e22d0f4f71 included one small
    change related to that, but that was just the tip of the iceberg. We
    need to store and handle the full wider filenames correctly in the
    pageserver.
    
    Includes test, which passes on v16, failed on v17 before this commit,
    and with this commit, passes on v17 too.

--- pageserver/src/basebackup.rs
+++ pageserver/src/basebackup.rs
@@ -32,7 +32,6 @@ use postgres_ffi::dispatch_pgversion;
 use postgres_ffi::pg_constants::{DEFAULTTABLESPACE_OID, GLOBALTABLESPACE_OID};
 use postgres_ffi::pg_constants::{PGDATA_SPECIAL_FILES, PG_HBA};
 use postgres_ffi::relfile_utils::{INIT_FORKNUM, MAIN_FORKNUM};
-use postgres_ffi::TransactionId;
 use postgres_ffi::XLogFileName;
 use postgres_ffi::PG_TLI;
 use postgres_ffi::{BLCKSZ, RELSEG_SIZE, WAL_SEGMENT_SIZE};
@@ -256,9 +255,7 @@ where
         let lazy_slru_download = self.timeline.get_lazy_slru_download() && !self.full_backup;
 
         let pgversion = self.timeline.pg_version;
-        let subdirs = dispatch_pgversion!(pgversion, {
-            &pgv::bindings::PGDATA_SUBDIRS[..]
-        });
+        let subdirs = dispatch_pgversion!(pgversion, { &pgv::bindings::PGDATA_SUBDIRS[..] });
 
         // Create pgdata subdirs structure
         for dir in subdirs.iter() {
@@ -611,7 +608,7 @@ where
     //
     // Extract twophase state files
     //
-    async fn add_twophase_file(&mut self, xid: TransactionId) -> Result<(), BasebackupError> {
+    async fn add_twophase_file(&mut self, xid: u64) -> Result<(), BasebackupError> {
         let img = self
             .timeline
             .get_twophase_file(xid, self.lsn, self.ctx)
